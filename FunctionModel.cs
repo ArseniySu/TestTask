4aaf2ce6557e8192557deb7f4b916363974a49d1
Сулима Арсений <arsey.sulima2013@yandex.ru>
2023-04-19 11:15:31 +0700

    Добавьте файлы проекта.

---
+++ FunctionModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal class FunctionModel
+    {
+        /// <summary>Имя Функции
+        public string Name { get; }
+
+        /// <summary>Делегат Функции
+        public Func<double, double, double> Function { get; }
+
+        /// <summary>Коэффициент A
+        public double A { get; set; }
+
+        /// <summary>Коэффициент B
+        public double B { get; set; }
+
+        /// <summary>Коэффициент C
+        public double C { get; set; }
+
+        public IReadOnlyList<double> Arguments { get; }
+
+        /// <summary>Создаёт экземпляр <see cref="FunctionModel"
+        /// <param name="name">Имя Функции
+        /// <param name="function">Делегат Функции.</param>
+        public FunctionModel(string name, IEnumerable<double> arguments, Func<double, double, double, double, double, double> function)
+        {
+            Name = name;
+            Arguments = arguments.ToList().AsReadOnly();
+            this.function = function ?? throw new ArgumentNullException(nameof(function));
+            Function = Calculate;
+        }
+        private readonly Func<double, double, double, double, double, double> function;
+        private double Calculate(double x, double y)
+            => function(A, B, C, x, y);
+    }
+}
---
+++ FunctionRowTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal class FunctionRowTable : BaseViewModel
+    {
+        private double _x;
+        private double _y;
+        private double? _f;
+
+        /// <summary>Значение X
+        public double X { get => _x; set => _x = value; }
+
+
+        /// <summary>Значение Y.</summary>
+        public double Y { get => _y; set => _y = value; }
+
+        /// <summary>Значение Функции для текущих значений <see cref="X"/> и <see cref="Y"
+        public double? F { get => _f; private set => _f = value; }
+
+
+        private FunctionModel function;
+
+        /// <summary>Задаёт Функцию от двух аргументов
+        /// <param name="function">Функция от двух аргументов
+        public void SetFunction(FunctionModel function)
+        {
+            this.function = function ?? throw new ArgumentNullException(nameof(function));
+            F = function?.Function(X, Y);
+        }
+
+        protected void OnPropertyChanged(string propertyName, object oldValue, object newValue)
+        {
+            OnPropertyChanged(propertyName, oldValue, newValue);
+
+            // Пересчёт значения Функции если изменилось значение X или Y.
+            if (propertyName == nameof(X) || propertyName == nameof(Y))
+                F = function?.Function(X, Y);
+        }
+    }
+}
